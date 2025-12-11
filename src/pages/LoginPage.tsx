04f6763f46a92049d91a4e9d8b96c7069a6a835b
Castro Monzon Martin <mhcastromonzon@cordoba.gov.ar>
2025-12-11 11:26:01 -0300

    fix/Ajustar LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,14 +5,16 @@ import WaveDivider from '../components/ui/WaveDivider';
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-white font-montserrat">
-      <div className="flex-1 flex items-center justify-center px-4 py-12">
+    <div className="min-h-screen flex flex-col bg-white font-montserrat overflow-x-hidden">
+      <div className="flex-1 flex items-center justify-center px-4 py-8 relative z-20">
         <div className="w-full max-w-md">
           <LoginCard />
         </div>
       </div>
-      <WaveDivider />
-      <Footer />
+      <div className="mt-auto relative z-10">
+        <WaveDivider />
+        <Footer />
+      </div>
     </div>
   );
 };
